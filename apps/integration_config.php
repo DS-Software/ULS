92db6006cd2e518794cd907a01a38e615bdd667c
DS Software <artemsoftware@mail.ru>
2022-02-24 23:01:14 +0400

    Updated configuration
    Changed config files, added .gitignore for config.php, updated the information about configuration steps.

--- apps/integration_config.php
+++ apps/integration_config.php
@@ -1,13 +1,6 @@
-<?php
-	/*
-		This flag enables or disables the ability
-		to create new integration apps.
-	*/
-
-	$enable_creation = true;
+<?php
+	$enable_creation = true;
 	$int_url = $login_url . "/apps";
-	
-	$delete_projects_on_inactivity = true;
-	$deletion_timeout = 8035200;
-	
+	$delete_projects_on_inactivity = false;
+	$deletion_timeout = 8035200;
 ?>
