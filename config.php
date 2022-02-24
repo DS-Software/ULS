8d5dd6eaea684f0aeb044367fad0a5cccf45a9f3
DS Software <artemsoftware@mail.ru>
2022-02-24 22:40:51 +0400

    config.php cleanup
    Cleaned up config, so it does not contain unused variables anymore.

--- config.php
+++ config.php
@@ -1,77 +1,31 @@
 <?php
 	if (isset($_SERVER['HTTP_CF_CONNECTING_IP'])) { $_SERVER['REMOTE_ADDR'] = $_SERVER['HTTP_CF_CONNECTING_IP']; }
 	
-	$allowed_origins = array(
-		0 => "https://ds-software.xyz",
-		1 => "https://alpha.ds-software.xyz",
-		2 => "https://gp.ds-software.xyz",
-		3 => "https://dev.ds-software.xyz",
-		4 => "https://artemdevvk.ml",
-		5 => "https://lk.ds-software.xyz"
-	);
-	
 	$maintenance_mode = false;
 
-	$login_site = "https://login.ds-software.xyz";
+	$login_site = "https://example.com/login";
 	
 	$domain_name = "/"; /*    / is default  */
 	
 	$session_length = 32;
 	
-	/*
-		Variables below this point
-		are SECURED variables.
-	*/
+	$service_key = "Very_Long_Service_Key";
 	
-	$service_key = ''; 
-	
-	$encryption_key = "";
-	
-	$service_oauth = "";
-	
-	$service_verification = "";
+	$encryption_key = "Long_Key_For_AES_Encryption.";
 	
 	$database = array(
-		'login' => '',
-		'password' => '',
-		'dbname' => '',
-		'hostname' => ''
+		'login' => 'database_login',
+		'password' => 'database_password',
+		'dbname' => 'database_auth',
+		'hostname' => 'localhost'
 	);
 
 	$email_settings = array(
-		'smtp' => '',
-		'port' => '',
-		'messageFrom' => '',
-		'login' => '',
-		'password' => ''
-	);
-	
-	$projects = array(
-		'chat' => array(
-			'url' => "https://ds-software.xyz/chat",
-			'api' => "https://ds-software.xyz/chat/api.php",
-			'key' => ''
-		),
-		'alpha_chat' => array(
-			'url' => "https://alpha.ds-software.xyz",
-			'api' => "https://alpha.ds-software.xyz/api.php",
-			'key' => ''
-		),
-		'gp_chat' => array(
-			'url' => "https://gp.ds-software.xyz/chat",
-			'api' => "https://gp.ds-software.xyz/chat/api.php",
-			'key' => ''
-		),
-		'license' => array(
-			'url' => "https://ds-software.xyz/license",
-			'api' => "https://ds-software.xyz/license/api.php",
-			'key' => ''
-		),
-		'dev_chat' => array(
-			'url' => "https://dev.ds-software.xyz/",
-			'api' => "https://dev.ds-software.xyz/api.php",
-			'key' => ''
-		)
+		'smtp' => 'your.smtp.provider',
+		'port' => '465',
+		'messageFrom' => 'Sender Name',
+		'login' => 'SMTP Login',
+		'password' => 'SMTP Password'
 	);
 	
 ?>
