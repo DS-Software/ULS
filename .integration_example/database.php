a99aabec65f2d4395d0ef60b2cec66a1aabe575b
DS Software <artemsoftware@mail.ru>
2022-06-19 18:13:39 +0400

    The endings of strings were messed up

--- .integration_example/database.php
+++ .integration_example/database.php
@@ -1,41 +1,40 @@
-<?php
-
-class database{
-	/*
-		A test Database Provider, use your own DB provider instead of this one
-	*/
-	
-	public function getUserIDByULSID($uls_id){
-		/*
-			Description:
-				Returns user's ID in yout project if the account
-				was created before, or NULL if it wasn't.
-			Parameters:
-				- $uls_id (ID of user's ULS account)
-			Returns:
-				- $user_id (ID of user's account in your project)
-				- null
-		*/
-		
-		$user_id = $uls_id;
-		
-		return $user_id;
-	}
-	
-	public function createNewUser($uls_id){
-		/*
-			Description:
-				Creates a user account in your database.
-			Parameters:
-				- $uls_id (ID of user's ULS account)
-			Returns:
-				- $user_id (ID of user's account in your project)
-		*/
-		
-		$user_id = $uls_id;
-		
-		return $user_id;
-	}
-}
-
+<?php
+
+class database{
+	/*
+		A test Database Provider, use your own DB provider instead of this one
+	*/
+	
+	public function getUserIDByULSID($uls_id){
+		/*
+		Description:
+			Returns user's ID in yout project if the account
+			was created before, or NULL if it wasn't.
+		Parameters:
+			- $uls_id (ID of user's ULS account)
+		Returns:
+			- $user_id (ID of user's account in your project)
+			- null
+		*/
+		$user_id = $uls_id;
+		
+		return $user_id;
+	}
+	
+	public function createNewUser($uls_id){
+		/*
+			Description:
+				Creates a user account in your database.
+			Parameters:
+				- $uls_id (ID of user's ULS account)
+			Returns:
+				- $user_id (ID of user's account in your project)
+		*/
+		
+		$user_id = $uls_id;
+		
+		return $user_id;
+	}
+}
+
 ?>
