8e7457e39a8ed2a1992d2c01fd5bd6c9a3325b40
DS Software <artemsoftware@mail.ru>
2022-10-15 22:03:59 +0400

    Prepared some stuff for PHP 8
    Changed some stuff to support PHP 8
    Also, realised that CAPTCHA change is required.

--- captcha/captcha.php
+++ captcha/captcha.php
@@ -34,7 +34,7 @@ class CAPTCHA{
 			while(true){
 				$this->keystring='';
 				for($i=0;$i<$length;$i++){
-					$this->keystring.=$allowed_symbols{random_int(0,strlen($allowed_symbols)-1)};
+					$this->keystring.=$allowed_symbols[random_int(0,strlen($allowed_symbols)-1)];
 				}
 				if(!preg_match('/cp|cb|ck|c6|c9|rn|rm|mm|co|do|cl|db|qp|qb|dp|ww/', $this->keystring)) break;
 			}
@@ -55,13 +55,13 @@ class CAPTCHA{
 				$transparent = (imagecolorat($font, $i, 0) >> 24) == 127;
 
 				if(!$reading_symbol && !$transparent){
-					$font_metrics[$alphabet{$symbol}]=array('start'=>$i);
+					$font_metrics[$alphabet[$symbol]]=array('start'=>$i);
 					$reading_symbol=true;
 					continue;
 				}
 
 				if($reading_symbol && $transparent){
-					$font_metrics[$alphabet{$symbol}]['end']=$i;
+					$font_metrics[$alphabet[$symbol]]['end']=$i;
 					$reading_symbol=false;
 					$symbol++;
 					continue;
@@ -80,7 +80,7 @@ class CAPTCHA{
 			$odd=random_int(0,1);
 			if($odd==0) $odd=-1;
 			for($i=0;$i<$length;$i++){
-				$m=$font_metrics[$this->keystring{$i}];
+				$m=$font_metrics[$this->keystring[$i]];
 
 				$y=(($i%2)*$fluctuation_amplitude - $fluctuation_amplitude/2)*$odd
 					+ random_int(-round($fluctuation_amplitude/3), round($fluctuation_amplitude/3))
